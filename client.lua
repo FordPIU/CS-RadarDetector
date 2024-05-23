db5c623bc4b227d1d939e5b2bf406f79415ae6fd
Caleb Brodock <brodock1234@gmail.com>
2024-05-23 09:44:18 -0700

    final rev 2

--- client.lua
+++ client.lua
@@ -27,12 +27,34 @@ function GetState(entity, key)
     return Entity(entity).state[key]
 end
 
+-- Function to play a sound
+local function playSound(soundName)
+    local soundId = GetSoundId()
+
+    PlaySoundFrontend(soundId, soundName, "DLC_HEIST_HACKING_SNAKE_SOUNDS", true)
+    ReleaseSoundId(soundId)
+end
+
+local function noObstructingTerrain(coord1, coord2, coord1_xBump)
+    coord1_xBump = coord1_xBump or 0
+    local rayId = StartExpensiveSynchronousShapeTestLosProbe(coord1[1], coord1[2], coord1[3] + coord1_xBump, coord2[1],
+        coord2[2],
+        coord2[3], 1, 0, 4)
+    local _, hit, _, _, _ = GetShapeTestResult(rayId)
+
+    if hit == 1 then
+        return false
+    else
+        return true
+    end
+end
+
 -- Function to find the closest vehicle with radar
 local function getClosestRadarCar(refPoint)
     local closestVeh, closestDist = nil, 1000.0
 
     for _, veh in pairs(GetGamePool("CVehicle")) do
-        if GetState(veh, "radarPowered") then
+        if GetState(veh, "radarPowered") and noObstructingTerrain(refPoint, GetEntityCoords(veh), 1.5) then
             local dist = #(GetEntityCoords(veh) - refPoint)
 
             if dist < closestDist then
@@ -42,14 +64,6 @@ local function getClosestRadarCar(refPoint)
     end
 
     return closestVeh, closestDist
-end
-
--- Function to play a sound
-local function playSound(soundName)
-    local soundId = GetSoundId()
-
-    PlaySoundFrontend(soundId, soundName, "DLC_HEIST_HACKING_SNAKE_SOUNDS", true)
-    ReleaseSoundId(soundId)
 end
 
 -- Main thread to handle radar detector functionality
@@ -73,10 +87,13 @@ Citizen.CreateThread(function()
             local radarVeh, radarDist = getClosestRadarCar(playerPos)
 
             if radarVeh then
-                if GetGameTimer() >= (lastBeepTime + (radarDist * 2)) then
+                if GetGameTimer() >= (lastBeepTime + (radarDist * 1.75)) then
                     playSound("Beep_Red")
                     lastBeepTime = GetGameTimer()
                 end
+
+                Wait(radarDist)
+                print(radarDist)
             else
                 Wait(100)
             end
