889ead1fbcf6e8cc1f707518bfc7abae89b9be20
Caleb Brodock <brodock1234@gmail.com>
2024-05-23 04:48:04 -0700

    final rev

--- client.lua
+++ client.lua
@@ -1,27 +1,38 @@
-local Radars = {}
-local DetectorOn = false
-
-RegisterNetEvent("Detector:RadarOnSync", function(radars)
-    Radars = radars
-end)
-
+-- Register the radar detector command
 RegisterCommand("rdetector", function(_, args, _)
     local arg1 = tostring(args[1]):lower()
+    local veh = GetVehiclePedIsIn(PlayerPedId(), false)
 
-    if arg1 == "on" then
-        DetectorOn = true
-    else
-        DetectorOn = false
+    if not DoesEntityExist(veh) then
+        print("You are not in a vehicle.")
+        return
     end
 
-    print(DetectorOn)
+    if arg1 == "on" then
+        SetState(veh, "radarDetector", true)
+        print("Radar detector activated.")
+    else
+        SetState(veh, "radarDetector", nil)
+        print("Radar detector deactivated.")
+    end
 end, false)
 
+-- Function to set the state of an entity
+function SetState(entity, key, value)
+    TriggerServerEvent("CSV:SETSTATE", NetworkGetNetworkIdFromEntity(entity), key, value)
+end
+
+-- Function to get the state of an entity
+function GetState(entity, key)
+    return Entity(entity).state[key]
+end
+
+-- Function to find the closest vehicle with radar
 local function getClosestRadarCar(refPoint)
-    local closestVeh, closestDist = nil, 500.0
+    local closestVeh, closestDist = nil, 1000.0
 
     for _, veh in pairs(GetGamePool("CVehicle")) do
-        if Entity(veh).state.radarPowered then
+        if GetState(veh, "radarPowered") then
             local dist = #(GetEntityCoords(veh) - refPoint)
 
             if dist < closestDist then
@@ -33,28 +44,49 @@ local function getClosestRadarCar(refPoint)
     return closestVeh, closestDist
 end
 
+-- Function to play a sound
+local function playSound(soundName)
+    local soundId = GetSoundId()
+
+    PlaySoundFrontend(soundId, soundName, "DLC_HEIST_HACKING_SNAKE_SOUNDS", true)
+    ReleaseSoundId(soundId)
+end
+
+-- Main thread to handle radar detector functionality
 Citizen.CreateThread(function()
+    local wasActive = false
+    local lastBeepTime = 0
+
     while true do
-        if DetectorOn then
+        local playerVeh = GetVehiclePedIsIn(PlayerPedId(), false)
+
+        if GetState(playerVeh, "radarDetector") then
             Wait(0)
 
-            local playerPoint = GetEntityCoords(PlayerPedId())
-            local radarVeh, radarDist = getClosestRadarCar(playerPoint)
+            if not wasActive then
+                playSound("Start")
+                wasActive = true
+                Wait(1000)
+            end
 
-            print(radarDist, radarDist * 2.5)
+            local playerPos = GetEntityCoords(PlayerPedId())
+            local radarVeh, radarDist = getClosestRadarCar(playerPos)
 
             if radarVeh then
-                local soundId = GetSoundId()
-
-                PlaySoundFromCoord(soundId, "Beep_Red", playerPoint[1], playerPoint[2], playerPoint[3],
-                    "DLC_HEIST_HACKING_SNAKE_SOUNDS", true, 5.0, true)
-                ReleaseSoundId(soundId)
-                Wait(radarDist * 2.5)
+                if GetGameTimer() >= (lastBeepTime + (radarDist * 2)) then
+                    playSound("Beep_Red")
+                    lastBeepTime = GetGameTimer()
+                end
             else
                 Wait(100)
             end
         else
-            Wait(500)
+            if wasActive then
+                playSound("Power_Down")
+                wasActive = false
+            end
+
+            Wait(1000)
         end
     end
 end)
